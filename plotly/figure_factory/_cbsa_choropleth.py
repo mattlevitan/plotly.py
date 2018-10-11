3e3a66683e6d2b1ed32c79cc91a855c73c9e05a1
Maxim Kupfer <mbk@maxim-mbp.local>
2018-10-10 19:35:33 -0700

    Removed _county_choropleth starter text

--- plotly/figure_factory/_cbsa_choropleth.py
+++ plotly/figure_factory/_cbsa_choropleth.py
@@ -21,25 +21,6 @@ shape_us_cbsa_2013 = os.path.join(abs_package_data_dir_path,
 
 df_shape_cbsa_2013 = gp.read_file(shape_us_cbsa_2013)
 
-filenames = ['tl_2013_us_cbsa.dbf',
-            'tl_2013_us_cbsa.shp',
-            'tl_2013_us_cbsa.shx']
-
-for j in range(len(filenames)):
-    filenames[j] = os.path.join(abs_package_data_dir_path, filenames[j])
-
-dbf = io.open(filenames[0], 'rb')
-shp = io.open(filenames[1], 'rb')
-shx = io.open(filenames[2], 'rb')
-
-reader = shapefile.Reader(shp=shp, shx=shx, dbf=dbf)
-
-attributes, geometry = [], []
-field_names = [i for i in df_shape_cbsa_2013.columns]
-for row in reader.shapeRecords():
-    geometry.append(shapely.geometry.shape(row.shape.__geo_interface__))
-    attributes.append(dict(zip(field_names, row.record)))
-
 gdf = gp.GeoDataFrame(data=attributes, geometry=geometry)
 
 
